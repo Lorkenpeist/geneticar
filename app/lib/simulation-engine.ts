01195222572d1e7e9f4eeb94ba90f56901dfab71
Lorkenpeist <58642611+Lorkenpeist@users.noreply.github.com>
2024-11-26 20:34:36 -0800

    [v0.6.0] Dynamically generate road (#7)
    * Generate road from a mathematical function curve
    
    * Dynamically generate road as cars approach
    
    * Update version to v0.6.0

--- app/lib/simulation-engine.ts
+++ app/lib/simulation-engine.ts
@@ -1,10 +1,7 @@
-import { Engine, Runner, Bodies, Composite, Events, World } from "matter-js";
+import { Engine, Runner, Composite, Events, World, Bodies } from "matter-js";
 import { Car, CarProperties } from "./car";
-import {
-  ROAD_SEGMENT_COUNT,
-  ROAD_SEGMENT_HEIGHT,
-  ROAD_SEGMENT_WIDTH,
-} from "./constants";
+import { ROAD_SEGMENT_HEIGHT, ROAD_SEGMENT_WIDTH } from "./constants";
+import { equationRoadGenerator } from "./equation-road-iterator";
 
 export interface SimulationOptions {
   carCount: number;
@@ -17,31 +14,52 @@ class SimulationEngine {
   cars: Car[] = [];
 
   start(options: SimulationOptions) {
+    // The equation that defines the curve of the road
+    const roadFunction = (x: number) => (-x * x) / (100 * ROAD_SEGMENT_WIDTH);
+
     this.cars = Array.from(Array(options.carCount), (_, i) => {
       const car = new Car(options.carProperties, {
-        x: 200 + 200 * i,
-        y: 600 - ROAD_SEGMENT_HEIGHT,
+        x: -200 * i,
+        y: roadFunction(0),
       });
       return car;
     });
-    const ground = Array.from({ length: ROAD_SEGMENT_COUNT }, (_, i) =>
-      Bodies.rectangle(
-        ROAD_SEGMENT_WIDTH * i,
-        600 - ROAD_SEGMENT_HEIGHT / 2,
-        ROAD_SEGMENT_WIDTH,
-        ROAD_SEGMENT_HEIGHT,
-        { isStatic: true },
-      ),
+
+    const startingRoad = Bodies.rectangle(
+      -5 * ROAD_SEGMENT_WIDTH,
+      roadFunction(0) + ROAD_SEGMENT_HEIGHT / 2,
+      10 * ROAD_SEGMENT_WIDTH,
+      ROAD_SEGMENT_HEIGHT,
+      { isStatic: true },
+    );
+
+    // The x coordinate of the last road segment.
+    // Used to check when cars approach the end of the road
+    // so more road can be generated.
+    let frontRoadPosition = startingRoad.position.x;
+
+    const roadGenerator = equationRoadGenerator(
+      (x) => (-x * x) / (100 * ROAD_SEGMENT_WIDTH),
     );
 
     Composite.add(this.engine.world, [
+      startingRoad,
       ...this.cars.map((car) => car.composite),
-      ...ground,
     ]);
 
     Events.on(this.engine, "beforeUpdate", () =>
       this.cars.forEach((car) => car.drive()),
     );
+
+    Events.on(this.engine, "afterUpdate", () => {
+      if (this.leader().front() > frontRoadPosition) {
+        const nextRoadSegment = roadGenerator.next();
+        if (!nextRoadSegment.done) {
+          Composite.add(this.engine.world, nextRoadSegment.value);
+          frontRoadPosition = nextRoadSegment.value.position.x;
+        }
+      }
+    });
 
     Runner.run(this.runner, this.engine);
   }
@@ -50,9 +68,11 @@ class SimulationEngine {
     // FIXME: update this after fixing Matter.js type definitions
     // @ts-expect-error Expected 3 arguments, but got 2.
     Events.off(this.engine, "beforeUpdate");
+    // @ts-expect-error Expected 3 arguments, but got 2.
+    Events.off(this.engine, "afterUpdate");
     Runner.stop(this.runner);
     Engine.clear(this.engine);
-    World.clear(this.engine.world, true);
+    World.clear(this.engine.world, false);
     this.cars = [];
   }
 
